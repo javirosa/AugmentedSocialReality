1123e6d6e1a1ffe6e1479fd0d4cdd433d114959d
Pablo Paredes <peparedes@gmail.com>
2011-04-26 05:36:46 +0000

    added preliminary narrative screen with tabs

---
+++ src/edu/berkeley/bid/narrativeadventures/NarrativeSelection.java
@@ -0,0 +1,18 @@
+package edu.berkeley.bid.narrativeadventures;
+
+import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class NarrativeSelection extends TabActivity {
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+  //  TabHost host = (TabHost) findViewById(R.id.);
+        
+    }
+
+}
