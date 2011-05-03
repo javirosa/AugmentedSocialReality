b66318bd9970750b950db72c2dce1c2a8ef6488c
Javier Rosa <javirosa1912@gmail.com>
2011-05-03 04:33:24 +0000

    Filter out people without phones.

--- src/edu/berkeley/bid/narrativeadventures/io/ContactListAgentSource.java
+++ src/edu/berkeley/bid/narrativeadventures/io/ContactListAgentSource.java
@@ -39,7 +39,14 @@ public class ContactListAgentSource implements AgentSourceI
                 Contacts.People.NUMBER,
                 Contacts.People.TIMES_CONTACTED,
                 Contacts.People._ID};
-        Cursor cur = cr.query(Contacts.People.CONTENT_URI, cols, null, null, Contacts.People.DEFAULT_SORT_ORDER);
+        String[] colsPhone = {Contacts.Phones.PERSON_ID,Contacts.Phones.NUMBER_KEY,Contacts.Phones.TYPE};
+        String[] colsPhoto = {Contacts.Photos.PERSON_ID,Contacts.Photos.DATA};
+        String hasNameSelector = Contacts.People.NAME + "<>\'\'";
+        Cursor cur = cr.query(Contacts.People.CONTENT_URI, cols, hasNameSelector, null, Contacts.People.DEFAULT_SORT_ORDER);
+        String hasNumberSelector = Contacts.Phones.PERSON_ID + "=?" 
+        + " AND "+ Contacts.Phones.TYPE+"="+Contacts.Phones.TYPE_MOBILE;
+        String hasPhotoSelector = Contacts.Photos.PERSON_ID + "=?";
+        byte[] empty = {};
         
         for (int i = 0; i < cur.getCount(); i++){
             //Pull data from the next contact
@@ -52,47 +59,53 @@ public class ContactListAgentSource implements AgentSourceI
             long _ID = cur.getLong(ID);
             
             //Get phone number from another database
-            String[] colsP = {Contacts.Phones.PERSON_ID,Contacts.Phones.NUMBER_KEY,Contacts.Phones.TYPE};
+            
             //Uri getPhoneURI =  android.content.ContentUris.withAppendedId(Contacts.Phones.CONTENT_URI,_ID);
-            String selector = Contacts.Phones.PERSON_ID + "=" + Long.toString(_ID) 
-                + " AND "+ Contacts.Phones.TYPE+"="+Contacts.Phones.TYPE_MOBILE; 
+           
             Cursor curP = cr.query(
                     Contacts.Phones.CONTENT_URI,
-                    colsP,
-                    selector,
-                    null,
+                    colsPhone,
+                    hasNumberSelector,
+                    new String[] {Long.toString(_ID)},
                     null);
-            if (curP.getCount() > 0) {
+            if (curP.getCount() > 0) {                      //If user has a number
                 curP.moveToFirst();
                 number = curP.getString(1);
                 char[] out = number.toCharArray();
-                for (int j = 0; j < out.length/2; j++) {  //Reverse 
+                for (int j = 0; j < out.length/2; j++) {    //Reverse 
                     char tmp = out[j];
                     out[j] = out[out.length-j-1];
                     out[out.length-j-1] = tmp;
                 }
                 number = new String(out);
-            } else {
-               number = "";
+            
+                
+                //Get raw image
+                Cursor curPhoto = cr.query(Contacts.Photos.CONTENT_URI, colsPhoto, hasPhotoSelector, new String[] {Long.toString(_ID)}, null);
+                curPhoto.moveToFirst();
+                byte[] bmp = curPhoto.getBlob(1);
+                if (bmp == null) {
+                    bmp = empty;
+                }
+                
+                /*/Load image in Bitmap format and convert to byte[]
+                /ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                byte[] bmp; 
+                Uri personURI = android.content.ContentUris.withAppendedId(Contacts.People.CONTENT_URI,cur.getLong(ID));
+                Bitmap btmap = Contacts.People.loadContactPhoto(activity.getApplicationContext(),personURI, R.drawable.icon, null);
+                btmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+                bmp = baos.toByteArray();*/
+                
+                Agent newAgent = new Agent();
+                newAgent.name = name;
+                newAgent.number = number;
+                Log.d("ContactListAgentSource","phone number:"+number);
+                newAgent.photo = bmp;
+                newAgent.responsiveness = contactCount; 
+            
+                returnMe.add(newAgent);
             }
             curP.close();
-            
-            //Load image in Bitmap format and convert to byte[]
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            byte[] bmp; 
-            Uri personURI = android.content.ContentUris.withAppendedId(Contacts.People.CONTENT_URI,cur.getLong(ID));
-            Bitmap btmap = Contacts.People.loadContactPhoto(activity.getApplicationContext(),personURI, R.drawable.icon, null);
-            btmap.compress(Bitmap.CompressFormat.PNG,100, baos);
-            bmp = baos.toByteArray();
-            
-            Agent newAgent = new Agent();
-            newAgent.name = name;
-            newAgent.number = number;
-            Log.d("ContactListAgentSource","phone number:"+number);
-            newAgent.photo = bmp;
-            newAgent.responsiveness = contactCount; 
-            
-            returnMe.add(newAgent);
         }
         
         cur.close();
