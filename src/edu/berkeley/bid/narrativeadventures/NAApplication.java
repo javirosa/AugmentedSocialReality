7282b7ce50bb5c35a7269e6027a7ed08c20dec66
Javier Rosa <javirosa1912@gmail.com>
2011-05-24 16:54:33 -0700

    work please
    Change-Id: Iaa7ce939abc3b2f6c6be811745a9f310f79da130

--- src/edu/berkeley/bid/narrativeadventures/NAApplication.java
+++ src/edu/berkeley/bid/narrativeadventures/NAApplication.java
@@ -14,6 +14,7 @@ import edu.berkeley.bid.narrativeadventures.model.Problem;
 public class NAApplication extends Application 
 {
 
+    //Fucking hell
     Problem currentProblem = null;
     ArrayList<Problem> runningProblems;
     ArrayList<Narrative> prototypeNarratives;
